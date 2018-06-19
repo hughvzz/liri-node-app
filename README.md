71a5a72f70bd88a158bfaf085110358035b513d1
HugoVzz <hughvzz@gmail.com>
2018-06-18 22:35:16 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # liri-node-app
+
+* Commands to use Liri App
+
+- Twitter: node liri.js my-tweets
+    displays 20 tweets and date of creation
+- Spotify: node liri.js spotify-this-song '<song name here>'
+    displays song information
+- OMDB: node liri.js movie-this '<movie name here>'
+    displays movie information
+- Random: node liri.js do-what-it-says
+    displays song information from random.txt file
---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+    consumer_key: 'yT3KPpBExbAqFOvek8j0K4qMa',
+    consumer_secret: 'zuLwI6p840YiDIZMq6D6iHKI4DjugUB7gTbD6sMfzgvvBBbOX5',
+    access_token_key: '1945133678-bLaMTIDFga7NRttvj2H3kWHfe8Pko3ezzB6ZTcq',
+    access_token_secret: 'XXa4UJ2Yi8JJKMvtufabxvLzDuxwRePWRi2dFrocUOAYM',
+  }
+  
+  exports.spotifyKeys = {
+      id: '2d6a6d1d7e79422cb959a723b18a997d',
+      secret: '6cfde6b3eac2464197a4aa336d956cb0',
+  }
